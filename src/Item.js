54aea8c66da3e5fb949980e9d3fd5947bf5ad20d
DrantDumani <darnjames@gmail.com>
2023-03-19 21:39:53 -0400

    Item component and tests added

---
+++ src/Item.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { Link, useParams } from "react-router-dom";
+
+function Item({ clickHandler, changeHandler, incHandler, decHandler }) {
+  const { itemId } = useParams();
+  return (
+    <div>
+      <label htmlFor="item-amount">Quantity:</label>
+      <input type="number" id="item-amount" onChange={changeHandler} />
+      <button onClick={incHandler}>+</button>
+      <button onClick={decHandler}>-</button>
+      <Link path="" onClick={clickHandler}>
+        Add to Cart
+      </Link>
+    </div>
+  );
+}
+
+export default Item;
