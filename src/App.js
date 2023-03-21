1fea5114db520eb9cc1ba94bf0d5d3e5f2e44dd3
DrantDumani <darnjames@gmail.com>
2023-03-20 22:35:04 -0400

    Removed state from item component

--- src/App.js
+++ src/App.js
@@ -10,20 +10,67 @@ import images from "./images";
 function App() {
   const [itemAmount, setItemAMount] = useState(0);
   const [cartItems, setCartItems] = useState([]);
+  const [currentItem, setCurrentItem] = useState({
+    id: "placeholder",
+    quantity: 1,
+  });
 
-  const addToKart = (num, itemObj) => {
+  const displayCurrentItem = (id) => {
+    setCurrentItem({ ...itemDB.find((item) => item.id === id), quantity: 1 });
+  };
+
+  const changeHandler = (event) => {
+    const { value } = event.target;
+    if (!/\D/g.test(value) && value.length > 0) {
+      setCurrentItem((curr) => {
+        return { ...curr, quantity: Number(value) };
+      });
+      validateMaxQuantity();
+    } else if (value === "") {
+      setCurrentItem((curr) => {
+        return { ...curr, quantity: 1 };
+      });
+    }
+  };
+
+  const incHandler = () => {
+    setCurrentItem((curr) => {
+      return { ...curr, quantity: curr.quantity + 1 };
+    });
+    validateMaxQuantity();
+  };
+
+  const decHandler = (limit) => {
+    setCurrentItem((curr) => {
+      if (curr.quantity - 1 > limit) {
+        return { ...curr, quantity: curr.quantity - 1 };
+      } else return { ...curr, quantity: 1 };
+    });
+  };
+
+  const validateMaxQuantity = () => {
+    setCurrentItem((curr) => {
+      if (curr.quantity > 999) {
+        return { ...curr, quantity: 999 };
+      }
+      return curr;
+    });
+  };
+
+  const addToKart = () => {
     // if the item is already in the kart, increase its quantity by the quantity instead
-    const inCart = cartItems.findIndex((item) => item.id === itemObj.id);
+    const inCart = cartItems.findIndex((item) => item.id === currentItem.id);
     if (inCart !== -1) {
       const updatedCart = [...cartItems];
-      updatedCart[inCart].quantity += num;
-      return;
+      updatedCart[inCart].quantity += currentItem.quantity;
+      setCartItems(updatedCart);
+    } else {
+      // const pushToKart = { ...itemObj, quantity: num };
+      setCartItems((currentCart) => {
+        return [...currentCart, currentItem];
+      });
+      console.log(cartItems);
     }
-    const pushToKart = { ...itemObj, quantity: num };
-    setCartItems((currentCart) => {
-      return [...currentCart, pushToKart];
-    });
-    console.log(cartItems);
   };
 
   const itemDB = [
@@ -45,7 +92,16 @@ function App() {
         <Route path="/shop" element={<Shop db={itemDB} />} />
         <Route
           path="/shop/:itemId"
-          element={<Item db={itemDB} clickHandler={addToKart} />}
+          element={
+            <Item
+              onMount={displayCurrentItem}
+              clickHandler={addToKart}
+              changeHandler={changeHandler}
+              incHandler={incHandler}
+              decHandler={decHandler}
+              item={currentItem}
+            />
+          }
         />
         <Route path="/cart" element={<ShoppingKart itemList={cartItems} />} />
       </Routes>
--- src/Item.js
+++ src/Item.js
@@ -1,72 +1,74 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 import { Link, useParams } from "react-router-dom";
 
 //show the price, name, descImg, and desc of the item that was used to link here.
 
-function Item({ clickHandler, db }) {
+function Item({
+  clickHandler,
+  onMount,
+  changeHandler,
+  incHandler,
+  decHandler,
+  item,
+}) {
   const { itemId } = useParams();
-  const [currentItem, setCurrentItem] = useState({
-    name: "",
-    price: "",
-    desc: "",
-    descImg: "",
-  });
-  const [itemAmount, setItemAmount] = useState(1);
+  // const [currentItem, setCurrentItem] = useState({
+  //   name: "",
+  //   price: "",
+  //   desc: "",
+  //   descImg: "",
+  // });
+  // const [itemAmount, setItemAmount] = useState(1);
 
-  const validateMaxQuantity = () => {
-    setItemAmount((curr) => {
-      if (curr > 999) {
-        return 999;
-      }
-      return curr;
-    });
-  };
+  // const validateMaxQuantity = () => {
+  //   setItemAmount((curr) => {
+  //     if (curr > 999) {
+  //       return 999;
+  //     }
+  //     return curr;
+  //   });
+  // };
 
-  const changeHandler = (event) => {
-    const { value } = event.target;
-    if (!/\D/g.test(value) && value.length > 0) {
-      setItemAmount(Number(value));
-      validateMaxQuantity();
-    } else if (value === "") {
-      setItemAmount(1);
-    }
-  };
+  // const changeHandler = (event) => {
+  //   const { value } = event.target;
+  //   if (!/\D/g.test(value) && value.length > 0) {
+  //     setItemAmount(Number(value));
+  //     validateMaxQuantity();
+  //   } else if (value === "") {
+  //     setItemAmount(1);
+  //   }
+  // };
 
-  const incHandler = () => {
-    setItemAmount((curr) => curr + 1);
-    validateMaxQuantity();
-  };
+  // const incHandler = () => {
+  //   setItemAmount((curr) => curr + 1);
+  //   validateMaxQuantity();
+  // };
 
-  const decHandler = () => {
-    setItemAmount((curr) => {
-      if (curr - 1 > 0) {
-        return curr - 1;
-      } else return 1;
-    });
-  };
+  // const decHandler = () => {
+  //   setItemAmount((curr) => {
+  //     if (curr - 1 > 0) {
+  //       return curr - 1;
+  //     } else return 1;
+  //   });
+  // };
 
   useEffect(() => {
-    for (let item of db) {
-      if (item.id === itemId) {
-        setCurrentItem(item);
-        return;
-      }
-    }
+    onMount(itemId);
   }, []);
 
   return (
     <div>
-      <img src={currentItem.descImg} alt="description" />
-      <p>{currentItem.desc}</p>
-      <p data-testid="item-name">{currentItem.name}</p>
-      <p data-testid="item-price">{currentItem.price}</p>
+      <img src={item.descImg} alt="description" />
+      <p>{item.desc}</p>
+      <p data-testid="item-name">{item.name}</p>
+      <p data-testid="item-price">{item.price}</p>
       <label htmlFor="item-amount">Quantity:</label>
       <input
         id="item-amount"
         onChange={(e) => {
           changeHandler(e);
         }}
-        value={itemAmount}
+        value={item.quantity}
         maxLength="3"
       />
       <button onClick={incHandler}>+</button>
@@ -74,7 +76,7 @@ function Item({ clickHandler, db }) {
       <Link
         to="/cart"
         onClick={() => {
-          clickHandler(itemAmount, currentItem);
+          clickHandler();
         }}
       >
         Add to Cart
