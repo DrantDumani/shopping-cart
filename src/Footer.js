49d2e7c6b2711d7a59562b6ea356b6a8e60903a0
DrantDumani <darnjames@gmail.com>
2023-03-22 01:36:12 -0400

    Completed project

---
+++ src/Footer.js
@@ -0,0 +1,15 @@
+function Footer() {
+  return (
+    <footer className="footer">
+      <a
+        className="github-link"
+        href="https://github.com/DrantDumani/shopping-cart"
+        target="_blank"
+      >
+        Github
+      </a>
+    </footer>
+  );
+}
+
+export default Footer;
--- src/images.js
+++ src/images.js
@@ -2,12 +2,28 @@ import descCrabTank from "./assets/S3_promo_screenshot_Crab_Tank_03.jpg";
 import crabTank from "./assets/S3_Weapon_Special_Crab_Tank.png";
 import waveBreaker from "./assets/S3_Weapon_Special_Wave_Breaker.png";
 import descWaveBreaker from "./assets/S3_Wave_Breaker_Promotional_3D_Render.jpg";
+import trizooka from "./assets/trizooka.png";
+import descTrizooka from "./assets/trizookaDesc.jpg";
+import reefSlider from "./assets/reefSlider.png";
+import descReefSlider from "./assets/descReefSlider.jpg";
+import inkjet from "./assets/inkjet.png";
+import descInkjet from "./assets/descInkjet.jpg";
+import inkstrike from "./assets/inkstrike.png";
+import descInkstrike from "./assets/descInkstrike.jpg";
 
 const images = {
   descCrabTank,
   crabTank,
   waveBreaker,
   descWaveBreaker,
+  trizooka,
+  descTrizooka,
+  reefSlider,
+  descReefSlider,
+  inkjet,
+  descInkjet,
+  inkstrike,
+  descInkstrike,
 };
 
 export default images;
